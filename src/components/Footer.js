75aa0a78955fbe54f2790f5bacffa79d5eb0b6fb
Angel Meza <undead.85@gmail.com>
2019-09-27 16:27:40 -0300

    Add theme

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Footer = (props) => (
+    <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
+        <p className="copyright">&copy; Cuidándote Siempre 2019. <a href="https://undead85.github.io">Developer</a>. Design: <a href="https://html5up.net">HTML5 UP</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a></p>
+    </footer>
+)
+
+Footer.propTypes = {
+    timeout: PropTypes.bool
+}
+
+export default Footer
---
+++ src/components/Main.js
@@ -0,0 +1,184 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import pic01 from '../images/pic01.jpg'
+import pic02 from '../images/pic02.jpg'
+import pic03 from '../images/pic03.jpg'
+
+class Main extends React.Component {
+  render() {
+    let close = (
+      <div
+        className="close"
+        onClick={() => {
+          this.props.onCloseArticle()
+        }}
+      ></div>
+    )
+
+    return (
+      <div
+        ref={this.props.setWrapperRef}
+        id="main"
+        style={this.props.timeout ? { display: 'flex' } : { display: 'none' }}
+      >
+        <article
+          id="intro"
+          className={`${this.props.article === 'intro' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+          }`}
+          style={{ display: 'none' }}
+        >
+          <h2 className="major">Intro</h2>
+          <span className="image main">
+            <img src={pic01} alt="" />
+          </span>
+          <p>
+            Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin
+            aliquam facilisis ante interdum congue. Integer mollis, nisl amet
+            convallis, porttitor magna ullamcorper, amet egestas mauris. Ut
+            magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas.
+            By the way, check out my <a href="#work">awesome work</a>.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+            dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora
+            torquent per conubia nostra, per inceptos himenaeos. Etiam tristique
+            libero eu nibh porttitor fermentum. Nullam venenatis erat id
+            vehicula viverra. Nunc ultrices eros ut ultricies condimentum.
+            Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae
+            dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in
+            lectus. Pellentesque habitant morbi tristique senectus et netus et
+            malesuada fames ac turpis egestas. In non lorem sit amet elit
+            placerat maximus. Pellentesque aliquam maximus risus, vel sed
+            vehicula.
+          </p>
+          {close}
+        </article>
+
+        <article
+          id="work"
+          className={`${this.props.article === 'work' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+          }`}
+          style={{ display: 'none' }}
+        >
+          <h2 className="major">Servicios</h2>
+          <span className="image main">
+            <img src={pic02} alt="" />
+          </span>
+          <p>
+            Cuidados de enfermeria en domicilio 24/7.
+            <ul>
+              <li>Adulto mayor</li>
+              <li>Post operados</li>
+              <li>Recién nacidos</li>
+              <li>Pre-escolares</li>
+            </ul>
+          </p>
+          <p>
+            Llegamos a las ciudades de Valparaiso, Viña del mar, Quilpué, 
+            Villa Alemana, Peñablanca, Limache y Olmué.
+          </p>
+          {close}
+        </article>
+
+        <article
+          id="about"
+          className={`${this.props.article === 'about' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+          }`}
+          style={{ display: 'none' }}
+        >
+          <h2 className="major">Precios</h2>
+          <span className="image main">
+            <img src={pic03} alt="" />
+          </span>
+          <p>
+            Cuidado de abuelitos $35.000 por 8 horas.<br/>
+            Precio por hora: $5.000.
+            <br/><br/>
+            <p style={{fontSize: '0.6rem', letterSpacing: '0.2rem', opacity: '0.75'}}>
+              Valores sujetos a cambios</p>
+          </p>
+          {close}
+        </article>
+
+        <article
+          id="contact"
+          className={`${this.props.article === 'contact' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+          }`}
+          style={{ display: 'none' }}
+        >
+          <h2 className="major">Contáctame</h2>
+          <p>
+            Celular : <a href="tel:+56950453157">9-504-531-57</a> 
+            <br/>Preguntar por srta. Carolina.
+          </p>
+          {/* <form method="post" action="#">
+            <div className="field half first">
+              <label htmlFor="name">Name</label>
+              <input type="text" name="name" id="name" />
+            </div>
+            <div className="field half">
+              <label htmlFor="email">Email</label>
+              <input type="text" name="email" id="email" />
+            </div>
+            <div className="field">
+              <label htmlFor="message">Message</label>
+              <textarea name="message" id="message" rows="4"></textarea>
+            </div>
+            <ul className="actions">
+              <li>
+                <input type="submit" value="Send Message" className="special" />
+              </li>
+              <li>
+                <input type="reset" value="Reset" />
+              </li>
+            </ul>
+          </form> */}
+          <ul className="icons">
+            {/* <li>
+              <a
+                href="#"
+                className="icon fa-twitter"
+              >
+                <span className="label">Twitter</span>
+              </a>
+            </li>
+            <li>
+              <a href="#" className="icon fa-facebook">
+                <span className="label">Facebook</span>
+              </a>
+            </li> */}
+            <li>
+              <a href="https://www.instagram.com/cuidandote_siempre/" className="icon fa-instagram">
+                <span className="label">Instagram</span>
+              </a>
+            </li>
+            {/* <li>
+              <a
+                href="https://github.com/codebushi/gatsby-starter-dimension"
+                className="icon fa-github"
+              >
+                <span className="label">GitHub</span>
+              </a>
+            </li> */}
+          </ul>
+          {close}
+        </article>
+      </div>
+    )
+  }
+}
+
+Main.propTypes = {
+  route: PropTypes.object,
+  article: PropTypes.string,
+  articleTimeout: PropTypes.bool,
+  onCloseArticle: PropTypes.func,
+  timeout: PropTypes.bool,
+  setWrapperRef: PropTypes.func.isRequired,
+}
+
+export default Main
